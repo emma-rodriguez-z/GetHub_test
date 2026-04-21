7d8e6176907002b735241f6ab5b94f69c6f0a5a6
emma-rodriguez-z <emmarodriguez@bren.ucsb.edu>
2026-04-21 08:35:22 -0700

    Initial commit

---
+++ week_1/script/lecture_3_test_gethub.R
@@ -0,0 +1,27 @@
+# Push to Get Hub test 
+
+install.packages("usethis")
+usethis::git_sitrep()
+
+
+# Create a github repository 
+
+install.packages("usethis")
+usethis::use_git()        # initializes git
+usethis::use_github()     # creates the GitHub repo and links it
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
